1761e770442754da7717a7a9ff7ef4dfdcbfada4
samer-nasr <samernasr1@live.com>
2024-12-24 04:32:58 +0200

    repair conflict

--- routes/api.php
+++ routes/api.php
@@ -7,19 +7,22 @@ use App\Http\Controllers\API\RegisteredUserConstroller;
 use App\Http\Controllers\API\ReviewControlle;
 use App\Http\Controllers\API\ServiceController;
 use App\Http\Controllers\API\UserController;
-
+use App\Http\Controllers\api\WorkersController;
 use Illuminate\Auth\TokenGuard;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-
+// Auth Apis
 Route::post('/register', [RegisteredUserConstroller::class, 'store']);
 Route::post('/login', [AuthenticatedSessionConstroller::class, 'login']);
-Route::post('/logout', [AuthenticatedSessionConstroller::class, 'logout']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthenticatedSessionConstroller::class, 'logout']);
+});
+
+// Route::middleware('auth:sanctum')->group(function () {
 // User API routes
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
@@ -29,10 +32,17 @@ Route::get('/services', [ServiceController::class, 'index']);
 Route::get('/services/{id}', [ServiceController::class, 'show']);
 Route::post('/add_service', [ServiceController::class, 'store']);
 
+//Workers API routes
+Route::get('/workers', [WorkersController::class, 'show']);
+Route::get('/workers/{service}', [WorkersController::class, 'show_by_service']);
+
 // Bookings API routes
 Route::post('/add_booking', [BookingController::class, 'store']);
 Route::get('/bookings', [BookingController::class, 'index']);
 Route::get('/bookings/{id}', [BookingController::class, 'show']);
+Route::get('/bookings/service_provider/{id}', [BookingController::class, 'show_service_provider_bookings']);
+Route::get('/bookings/customer/{id}', [BookingController::class, 'show_customer_bookings']);
+Route::get('/bookings_services', [BookingController::class, 'show_services_bookings']);
 
 
 // Payments API routes
@@ -46,13 +56,4 @@ Route::post('/add_review', [ReviewControlle::class, 'store']);
 Route::get('/get_reviews', [ReviewControlle::class, 'index']);
 Route::get('/get_reviews/{id}', [ReviewControlle::class, 'show']);
 
-});
-
-
-
-Route::get('/test', function () {
-    dd('Test Api');
-});
-
-// Route::get('/users', [UserController::class, 'index']);
-// Route::get('/bookings', [BookingController::class, 'index']);
+// });
